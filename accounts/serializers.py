8c22ec7672f84f84926b2cf5f3dc489e78804fcf
andreimocian <andrei_mocian@yahoo.com>
2024-11-02 18:28:50 +0200

    Fixed user accounts to match frontend

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -9,4 +9,4 @@ class UserCreateSerializerr(UserCreateSerializer):
     company = serializers.CharField(required=False, allow_null=True)
     class Meta(UserCreateSerializer.Meta):
         model = User
-        fields = ("id", "email", "first_name", "last_name", "company", "password", "is_dispatcher", "is_driver")
+        fields = ("id", "email", "name", "company", "password", "is_dispatcher", "is_driver")
