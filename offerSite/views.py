81a10ae6f707ae574860c2c8882a98a5b57da2bf
Leonardo Mosutan <leomos2005@gmail.com>
2025-03-19 00:21:39 +0200

    test

--- offerSite/views.py
+++ offerSite/views.py
@@ -4,16 +4,22 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
 import os
+import tempfile
 
 # Configurare Google Sheets API
-SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")  # ID-ul fișierului tău Google Sheets
-CREDENTIALS_FILE_json = os.getenv("CREDENTIALS_FILE")  # Calea către fișierul JSON descărcat
-CREDENTIALS_FILE = json.loads(CREDENTIALS_FILE_json)
-CREDENTIALS_FILE["private_key"] = CREDENTIALS_FILE["private_key"].replace("\\n", "\n")
+SPREADSHEET_ID = '1cJXwzEXl6QIAWzZ2tksgCp_8BrfYMXiODH7p3asbxG8'  # ID-ul fișierului tău Google Sheets
+CREDENTIALS_FILE = 'offerSite/offers-454115-2e54df6d665b.json'  # Calea către fișierul JSON descărcat
+#CREDENTIALS_FILE = json.loads(CREDENTIALS_FILE_json)
+#with tempfile.NamedTemporaryFile(mode="w", delete=False, suffix=".json") as temp_file:
+#    temp_file.write(CREDENTIALS_FILE_json)
+ #   file_path = temp_file.name  # Salvează calea fișierului
+
+#CREDENTIALS_FILE = file_path
+
 # Conectează-te la Google Sheets
 def connect_to_sheets():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(CREDENTIALS_FILE, scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, scope)
     client = gspread.authorize(creds)
     return client.open_by_key(SPREADSHEET_ID).sheet1  # Deschide Sheet1
 
@@ -28,7 +34,7 @@ def upload_to_google_sheets(request):
 
             sheet = connect_to_sheets()
             sheet.append_row([telefon, email, numar_soferi])
-
+            os.remove(file_path)  # Șterge fișierul temporar
             return JsonResponse({"message": "Datele au fost salvate în Google Sheets!"}, status=200)
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
