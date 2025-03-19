8ec49c1d1a99baabad61b5557a7fe9ea84235218
leogrozavu <leomos2005@gmail.com>
2025-03-19 11:09:33 +0200

    test

--- base/apps.py
+++ base/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+import os
 
 
 class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
+
+    def ready(self):
+        import atexit
+        atexit.register(self.cleanup)
+
+    def cleanup(self):
+        if os.path.exists("/tmp/google_credentials.json"):
+            os.remove("/tmp/google_credentials.json")
