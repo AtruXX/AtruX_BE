b6eb8e0278f7eb0a92ea6e0096d4466d7eb2a8cb
andreimocian <andrei_mocian@yahoo.com>
2025-04-01 23:09:16 +0300

    added truck and trailer functionality

--- base/models.py
+++ base/models.py
@@ -6,14 +6,45 @@ class Company(models.Model):
     name = models.CharField(max_length=100, unique=True)
     code = models.CharField(primary_key=True, null=False, max_length=100, unique=True)
     
-class Point(models.Model):
-    name = models.CharField(max_length=100)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+class Truck(models.Model):
+    license_plate = models.CharField(max_length=100, unique=True)
+    vin = models.CharField(max_length=100, unique=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_trucks')
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    next_service_date = models.DateField()
+    last_service_date = models.DateField()
+
+class Trailer(models.Model):
+    license_plate = models.CharField(max_length=100, unique=True)
+    vin = models.CharField(max_length=100, unique=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='company_trailers')
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    next_service_date = models.DateField()
+    last_service_date = models.DateField()
+
+class TruckDocument(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    truck = models.ForeignKey(Truck, on_delete=models.CASCADE, related_name='documents')
+    document = models.FileField(upload_to="truck_documents/", blank=False, null=False)
+    category = models.CharField(max_length=100, blank=True, null=True)
+    expiration_date = models.DateField(blank=True, null=True)
+
+class TrailerDocument(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    trailer = models.ForeignKey(Trailer, on_delete=models.CASCADE, related_name='documents')
+    document = models.FileField(upload_to="trailer_documents/", blank=False, null=False)
+    category = models.CharField(max_length=100, blank=True, null=True)
+    expiration_date = models.DateField(blank=True, null=True)
 
 class Transport(models.Model):
     driver = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='driver_transports')
     dispatcher = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='dispatcher_transports')
+    truck = models.ForeignKey(Truck, on_delete=models.CASCADE, related_name='transports', null=True, blank=True)
+    trailer = models.ForeignKey(Trailer, on_delete=models.CASCADE, related_name='transports', null=True, blank=True)
     status_truck = models.CharField(max_length=100)
     status_truck_text = models.CharField(max_length=255, blank=True, null=True)
     status_goods = models.CharField(max_length=100)
@@ -26,6 +57,11 @@ class Transport(models.Model):
     detraction = models.CharField(max_length=100)
     status_transport = models.CharField(max_length=100, default='not started')
 
+class Point(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
 class Route(models.Model):
     transport = models.ForeignKey(Transport, on_delete=models.CASCADE, related_name='routes')
     points = models.ManyToManyField(Point)
@@ -36,4 +72,3 @@ class TransportDocument(models.Model):
     transport = models.ForeignKey(Transport, on_delete=models.CASCADE, related_name='documents')
     document = models.FileField(upload_to="transport_documents/", blank=False, null=False)
     category = models.CharField(max_length=100, blank=True, null=True)
-
