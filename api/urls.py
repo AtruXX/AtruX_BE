358d3eadd444e09ce934dba567917acd78ca1687
Leonardo Mosutan <leomos2005@gmail.com>
2025-04-06 17:33:52 +0300

    fix

--- api/urls.py
+++ api/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('delete_cmr/', views.deleteCMR),
     path('update_cmr/', views.updateCMR),
     path("add_truck/", views.addTruck),
-    path("get_cmrs/", views.getCMRByTransport),
+    path("get_cmrs/<int:transport_id>", views.getCMRByTransport),
     path("upload_truck_document/", views.UploadTruckDocuments), #todo
     path("delete_truck/", views.deleteTruck),
     path("get_trucks/", views.getAllTrucks),
