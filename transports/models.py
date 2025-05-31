d777fca4d1b8b9224d814348fbcf54d95359b4c8
andreimocian <andrei_mocian@yahoo.com>
2025-05-31 16:13:51 +0300

    Added UIT for transports

--- transports/models.py
+++ transports/models.py
@@ -31,6 +31,7 @@ class Transport(models.Model):
     destination_city = models.CharField(max_length=100)
     goods_type = models.CharField(max_length=100)
     is_finished = models.BooleanField(default=False)
+    UIT = models.CharField(max_length=100, blank=True, null=True)
 
 class Route(models.Model):
     transport = models.ForeignKey(Transport, on_delete=models.CASCADE, related_name='routes')
